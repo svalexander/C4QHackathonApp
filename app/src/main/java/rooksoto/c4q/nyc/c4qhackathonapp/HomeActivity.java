75e22a79bed53784d8fde4604ced7227915cc08e
Rook Soto <rooksoto@gmail.com>
2017-02-18 10:54:34 -0500

    Skeleton

--- app/src/main/java/rooksoto/c4q/nyc/c4qhackathonapp/HomeActivity.java
+++ app/src/main/java/rooksoto/c4q/nyc/c4qhackathonapp/HomeActivity.java
@@ -3,11 +3,11 @@ package rooksoto.c4q.nyc.c4qhackathonapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
---
+++ app/src/main/java/rooksoto/c4q/nyc/c4qhackathonapp/SplashActivity.java
@@ -0,0 +1,17 @@
+package rooksoto.c4q.nyc.c4qhackathonapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rook on 2/18/17.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
