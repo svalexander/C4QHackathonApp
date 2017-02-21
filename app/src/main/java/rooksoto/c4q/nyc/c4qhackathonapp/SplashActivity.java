303798b522aedffe0492112d27527f60aa0fb77b
Akasha Archere <akasha.archer@gmaile.com>
2017-02-21 14:23:00 -0500

    Splash screen in place

--- app/src/main/java/rooksoto/c4q/nyc/c4qhackathonapp/SplashActivity.java
+++ app/src/main/java/rooksoto/c4q/nyc/c4qhackathonapp/SplashActivity.java
@@ -1,6 +1,8 @@
 package rooksoto.c4q.nyc.c4qhackathonapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,5 +15,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+
     }
 }
