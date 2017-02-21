52311d8b3ffa78b0bef5a454ce3e96b6cc679e2b
Akasha Archere <akasha.archer@gmaile.com>
2017-02-21 00:08:10 -0500

    Emergency/ Urgent Care maps in place

--- app/src/main/java/rooksoto/c4q/nyc/c4qhackathonapp/fragments/UrgentCareFragment.java
+++ app/src/main/java/rooksoto/c4q/nyc/c4qhackathonapp/fragments/UrgentCareFragment.java
@@ -3,8 +3,6 @@ package rooksoto.c4q.nyc.c4qhackathonapp.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +19,6 @@ public class UrgentCareFragment extends Fragment {
     private int page;
     private static final String INT_TAG = "one";
     private static final String TITLE_TAG = "urgFrag";
-    private RecyclerView recyclerView;
 
 
     public static Fragment newInstance(int page, String title) {
@@ -43,9 +40,6 @@ public class UrgentCareFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_urgent_care,container,false);
-
-        recyclerView = (RecyclerView) view.findViewById(R.id.urgent_care_rv);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         return view;
     }
